9e3ba59bca3f1cdef07c76eaa2944f0fdc14091d
Roni Cakmak <90357658+roniraijin@users.noreply.github.com>
2025-03-17 23:52:04 +0000

    the real branch sat update
    idk what happened with the other commit

--- test.2.py
+++ test.2.py
@@ -1,11 +1,33 @@
 def branching_sat_solve(clause_set,partial_assignment):
-    ...
+    
+
+    def iterate(clause_set, partial_assignment):
+        for i in partial_assignment:
+            for clause in clause_set:
+                if i in clause:
+                    clause.remove(i)
+        return partial_assignment
     literals = set()
     for clause in clause_set:
         for literal in clause:
             if literal not in literals:
                 literals.add(abs(literal))
-    print(literals)
+    for literal in literals:
+        for i in [True,False]:
+            clause_set_copy = clause_set.copy()
+            partial_assignment_copy = partial_assignment.copy()
+            if i:    
+                partial_assignment_copy.append(literal)
+            else:
+                partial_assignment_copy.append(-literal)
+            iterate(clause_set_copy, partial_assignment)
+
+
+
+
+        ...
+
+
 
 
 
