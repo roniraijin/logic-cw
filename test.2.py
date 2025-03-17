766ec2c3f482511186eb7c741d242c2c2bffafd2
CAKMAK <rfqf56@durham.ac.uk>
2025-03-17 23:50:01 +0000

    cooking with branch sat inshallah

--- test.2.py
+++ test.2.py
@@ -1,30 +1,13 @@
-def load_dimacs(file_name):
-    #file_name will be of the form "problem_name.txt"
-    with open(f'{file_name}', 'r') as d:
-        d = d.readlines()
-    print(d)
-    d.pop(0)
-    clause_set = []
-    for x in d:
-        temp = []  
-        for t in x.split():
-            if t != '0':
-                try:    
-                    temp.append(int(t))  
-                except ValueError:
-                    pass
-        if temp:  
-            clause_set.append(temp)
-    print(clause_set)
-    return(clause_set)
+def branching_sat_solve(clause_set,partial_assignment):
+    ...
+    literals = set()
+    for clause in clause_set:
+        for literal in clause:
+            if literal not in literals:
+                literals.add(abs(literal))
+    print(literals)
 
 
-def test():
-    print("Testing load_dimacs")
-    try:
-        dimacs = load_dimacs("sat.txt")
-        assert dimacs == [[1],[1,-1],[-1,-2]]
-        print("Test passed")
-    except:
-        print("Failed to correctly load DIMACS file")
-test()
+
+
+branching_sat_solve([[1],[2],[4],[-3,5]], [])
